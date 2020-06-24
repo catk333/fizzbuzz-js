b374b20f0dc9f237eeecf2c8a460d1af25e2b802
Catherine Waind <catherine.waind@bbc.co.uk>
2020-06-24 16:49:41 +0100

    Fizz buzz task 2 - completed

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,26 +5,50 @@ function fizzbuzz() {
 
      console.log(" Welcome to Fizz Buzz")
     
-     for (i=1;i<=20;i++) {
+     for (i=1;i<=100;i++) {
 
          let result = ""
+    
         
-         if (i % 15 === 0){
-            result = "FizzBuzz";
-         }
-         else if (i % 3 === 0){
+         
+         if (i % 3 === 0){
+
              result = "Fizz";
+            }
+         
+         if ( i % 5 === 0){
+            result = (result +"Buzz")
+            }
+    
+    
+        if ( i % 7 === 0){
+             result = (result + "Bang")
+            }
 
-         }
-         else if ( i % 5 === 0){
-            result ="Buzz";
-         }
-         else {
-             result= i
-         }
+        if ( i % 11 === 0){
+                result = "Bong"
+            }
+
+       if ( i % 13 === 0){
+              const firstB = result.search("B");
+              if (firstB === -1){
+                  result = result + "Fezz";
+              } else {
+                    resultFirstSplit = result.slice(0,firstB) 
+                    resultSecondSplit = result.slice(firstB)
+                    resultIfB = "Fezz"
+                    result = resultFirstSplit + resultIfB + resultSecondSplit 
+              }
+       }
+     if (result === "") {
+            result= i
+            }
+        
+
          console.log(result)
+        }   
+    
 
-    }
 
     
 }
