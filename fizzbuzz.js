9b227581d7c3e0f753b074fd178ae4db4196d217
Catherine Waind <catherine.waind@bbc.co.uk>
2020-06-24 11:24:27 +0100

    Fizz buzz task 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,10 +2,29 @@
 
 // Here, we create our main function.
 function fizzbuzz() {
-    
-    console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
+     console.log(" Welcome to Fizz Buzz")
+    
+     for (i=1;i<=20;i++) {
+
+         let result = ""
+        
+         if (i % 15 === 0){
+            result = "FizzBuzz";
+         }
+         else if (i % 3 === 0){
+             result = "Fizz";
+
+         }
+         else if ( i % 5 === 0){
+            result ="Buzz";
+         }
+         else {
+             console.log(result)
+         }
+
+
+    }
 
     
 }
